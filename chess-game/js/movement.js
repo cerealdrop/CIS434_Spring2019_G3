31cb97e8c1a642a79773402753f4268b925ab26a
etkline <48033949+etkline@users.noreply.github.com>
2019-04-06 17:51:34 -0400

    Update movement.js
    Basic Pawn Movement Implemented

--- chess-game/js/movement.js
+++ chess-game/js/movement.js
@@ -1,20 +1,48 @@
-var elem = "";
-var id = "";
-var backColor = "";
-function go(clicked_id) {
-    if (elem == "") {
-        if (document.getElementById(clicked_id).innerHTML != "") {
-            elem = document.getElementById(clicked_id).innerHTML;
-            id = clicked_id;
-            backColor = document.getElementById(id).style.backgroundColor;
-            document.getElementById(id).style.backgroundColor = "#98FB98";
+var piece = "";
+var pos = "";
+
+var backColors = [];
+var movs = [];
+
+function move(clicked_id) {
+
+    if (piece == "") {
+
+        var icon = getIconValue(clicked_id);
+        if (!isNaN(icon)) {
+            piece = Piece.getPiece(icon);
+            pos = clicked_id;
+
+            backColors.push(document.getElementById(pos).style.backgroundColor);
+            document.getElementById(pos).style.backgroundColor = "#98FB98"; //98FB98
+
+            movs = getMovementOptions(piece, pos);
+            highlightBackColors();
         }
     } else {
-        if (id != clicked_id) {
-            document.getElementById(clicked_id).innerHTML = elem;
-            document.getElementById(id).innerHTML = "";
+
+        if (movs.includes(clicked_id)) {
+            document.getElementById(clicked_id).innerHTML = "<h1>" + String.fromCharCode(piece.icon) + "</h1>";
+            document.getElementById(pos).innerHTML = "";
         }
-        document.getElementById(id).style.backgroundColor = backColor;
-        elem = "";
+        document.getElementById(pos).style.backgroundColor = backColors.shift();
+        piece = "";
+        pos = "";
+
+        clearHighlights();
     }
+}
+
+function highlightBackColors() {
+
+    for (var i = 0; i < movs.length; i ++) {
+        backColors.push(document.getElementById(movs[i]).style.backgroundColor);
+        document.getElementById(movs[i]).style.backgroundColor = "#BFFFFE";
+    }
+}
+
+function clearHighlights() {
+
+    while (movs.length > 0)
+        document.getElementById(movs.pop()).style.backgroundColor = backColors.pop();
 }
